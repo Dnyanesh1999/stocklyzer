c183990fbbc6d666a22caf9baf35d15d855c440c
Dnyanesh Mali <102671227+Dnyanesh1999@users.noreply.github.com>
2025-06-19 12:08:12 +0530

    feat: make navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import { AppBar, Toolbar, Box, Typography, Button } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Typography,
+  Button,
+  IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 import { Link, useLocation } from "react-router-dom";
 import TrendingUp from "@mui/icons-material/TrendingUp";
 import Settings from "@mui/icons-material/Settings";
 import FavoriteOutlined from "@mui/icons-material/FavoriteOutlined";
-import { SmartToy } from "@mui/icons-material";
+import { SmartToy, Menu } from "@mui/icons-material";
+import { useState } from "react";
 
 const navLinks = [
   { lable: "Dashboard", path: "/", icon: <TrendingUp /> },
@@ -14,6 +30,33 @@ const navLinks = [
 
 const Navbar = () => {
   const location = useLocation();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => () => {
+    setDrawerOpen(open);
+  };
+
+  const drawerContent = (
+    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+      <List>
+        {navLinks.map((link) => (
+          <ListItem key={link.path} disablePadding>
+            <ListItemButton
+              component={Link}
+              to={link.path}
+              selected={location.pathname === link.path}
+            >
+              <ListItemIcon>{link.icon}</ListItemIcon>
+              <ListItemText primary={link.lable} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <AppBar
       position="static"
@@ -33,34 +76,45 @@ const Navbar = () => {
         >
           Stocklyzer
         </Typography>
-        <Box display="flex" gap={4}>
-          {navLinks.map((link) => (
-            <Button
-              key={link.path}
-              component={Link}
-              to={link.path}
-              startIcon={link.icon}
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                color: "primary.contrastText",
-                padding: "8px 16px",
-                backgroundColor:
-                  location.pathname === link.path
-                    ? "primary.light"
-                    : "transparent",
-                borderRadius: "8px",
-                fontSize: "16px",
-                fontWeight: 500,
-                "&:hover": {
-                  backgroundColor: "primary.light",
-                },
-              }}
-            >
-              {link.lable}
-            </Button>
-          ))}
-        </Box>
+        {isMobile ? (
+          <>
+            <IconButton color="inherit" onClick={toggleDrawer(true)}>
+              <Menu />
+            </IconButton>
+            <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+              {drawerContent}
+            </Drawer>
+          </>
+        ) : (
+          <Box display="flex" gap={4}>
+            {navLinks.map((link) => (
+              <Button
+                key={link.path}
+                component={Link}
+                to={link.path}
+                startIcon={link.icon}
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  color: "primary.contrastText",
+                  padding: "8px 16px",
+                  backgroundColor:
+                    location.pathname === link.path
+                      ? "primary.light"
+                      : "transparent",
+                  borderRadius: "8px",
+                  fontSize: "16px",
+                  fontWeight: 500,
+                  "&:hover": {
+                    backgroundColor: "primary.light",
+                  },
+                }}
+              >
+                {link.lable}
+              </Button>
+            ))}
+          </Box>
+        )}
       </Toolbar>
     </AppBar>
   );
